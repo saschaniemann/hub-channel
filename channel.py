f1e544a6177bcbdf6a3fee6019e22dbe0683d85a
sniemann <sascha.niemann66@gmx.de>
2025-02-13 20:02:43 +0100

    setup

---
+++ channel.py
@@ -0,0 +1,202 @@
+"""channel.py - a simple message channel."""
+
+from flask import Flask, request, render_template, jsonify
+import json
+import requests
+
+
+# Class-based application configuration
+class ConfigClass(object):
+    """Flask application configuration class."""
+
+    # Flask settings
+    SECRET_KEY = "This is an INSECURE secret!! DO NOT use this in production!!"
+
+
+# Create Flask app
+app = Flask(__name__)
+app.config.from_object(__name__ + ".ConfigClass")  # Configuration
+app.app_context().push()  # Create an app context before initializing the app
+
+HUB_URL = "http://localhost:5555"
+HUB_AUTHKEY = "1234567890"
+CHANNEL_AUTHKEY = "0987654321"
+CHANNEL_NAME = "The One and Only Channel"
+CHANNEL_ENDPOINT = (
+    "http://localhost:5001"  # Don't forget to adjust in the bottom of the file
+)
+CHANNEL_FILE = "messages.json"
+CHANNEL_TYPE_OF_SERVICE = "aiweb24:chat"
+
+
+@app.cli.command("register")
+def register_command():
+    """Register the channel with the hub by sending a POST request.
+
+    This function sends the channel information to the hub server to
+    register it and create a new channel.
+
+    Returns:
+        None
+
+    """
+    global CHANNEL_AUTHKEY, CHANNEL_NAME, CHANNEL_ENDPOINT
+
+    # Send a POST request to server /channels
+    response = requests.post(
+        HUB_URL + "/channels",
+        headers={"Authorization": "authkey " + HUB_AUTHKEY},
+        data=json.dumps(
+            {
+                "name": CHANNEL_NAME,
+                "endpoint": CHANNEL_ENDPOINT,
+                "authkey": CHANNEL_AUTHKEY,
+                "type_of_service": CHANNEL_TYPE_OF_SERVICE,
+            }
+        ),
+    )
+
+    if response.status_code != 200:
+        print("Error creating channel: " + str(response.status_code))
+        print(response.text)
+        return
+
+
+def check_authorization(request):
+    """Check if the authorization header is valid.
+
+    Args:
+        request (flask.Request): The incoming request to check.
+
+    Returns:
+        bool: True if authorization is valid, False otherwise.
+
+    """
+    global CHANNEL_AUTHKEY
+    # Check if Authorization header is present
+    if "Authorization" not in request.headers:
+        return False
+    # Check if the authorization header is valid
+    if request.headers["Authorization"] != "authkey " + CHANNEL_AUTHKEY:
+        return False
+    return True
+
+
+@app.route("/health", methods=["GET"])
+def health_check():
+    """Check the health status of the channel.
+
+    Returns:
+        JSON: A JSON response containing the channel name if authorized,
+               or an error message if unauthorized.
+
+    """
+    global CHANNEL_NAME
+    if not check_authorization(request):
+        return "Invalid authorization", 400
+    return jsonify({"name": CHANNEL_NAME}), 200
+
+
+# GET: Return list of messages
+@app.route("/", methods=["GET"])
+def home_page():
+    """Return a list of messages in JSON format.
+
+    This function handles GET requests to fetch all messages.
+
+    Returns:
+        JSON: A JSON response containing the list of messages if authorized,
+               or an error message if unauthorized.
+
+    """
+    if not check_authorization(request):
+        return "Invalid authorization", 400
+    # Fetch channels from server
+    return jsonify(read_messages())
+
+
+# POST: Send a message
+@app.route("/", methods=["POST"])
+def send_message():
+    """Receive and store a new message in the channel.
+
+    This function handles POST requests to add a new message.
+
+    Returns:
+        str: A response indicating success ("OK") or an error message.
+
+    """
+    # Fetch channels from server
+    # Check authorization header
+    if not check_authorization(request):
+        return "Invalid authorization", 400
+    # Check if message is present
+    message = request.json
+    if not message:
+        return "No message", 400
+    if not "content" in message:
+        return "No content", 400
+    if not "sender" in message:
+        return "No sender", 400
+    if not "timestamp" in message:
+        return "No timestamp", 400
+    if "extra" in message:
+        extra = message["extra"]
+    else:
+        extra = None
+
+    # Add message to messages
+    messages = read_messages()
+    messages.append(
+        {
+            "content": message["content"],
+            "sender": message["sender"],
+            "timestamp": message["timestamp"],
+            "extra": extra,
+        }
+    )
+    save_messages(messages)
+    return "OK", 200
+
+
+def read_messages():
+    """Read messages from the messages file.
+
+    Returns:
+        list: A list of messages from the file, or an empty list if file not found.
+
+    """
+    global CHANNEL_FILE
+    try:
+        f = open(CHANNEL_FILE, "r")
+    except FileNotFoundError:
+        return []
+    try:
+        messages = json.load(f)
+    except json.decoder.JSONDecodeError:
+        messages = []
+    f.close()
+    return messages
+
+
+def save_messages(messages):
+    """Save a list of messages to the messages file.
+
+    Args:
+        messages (list): The list of messages to save.
+
+    Returns:
+        None
+
+    """
+    global CHANNEL_FILE
+    with open(CHANNEL_FILE, "w") as f:
+        json.dump(messages, f)
+
+
+# Start development web server
+# run flask --app channel.py register
+# to register channel with hub
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
---
+++ client.py
@@ -0,0 +1,133 @@
+"""Implementation of a client of a hub-channel application."""
+
+from flask import Flask, request, render_template, url_for, redirect
+import requests
+import urllib.parse
+import datetime
+
+app = Flask(__name__)
+
+HUB_AUTHKEY = "1234567890"
+HUB_URL = "http://localhost:5555"
+
+CHANNELS = None
+LAST_CHANNEL_UPDATE = None
+
+
+def update_channels():
+    """Fetch and update the list of channels from the server.
+
+    This function checks if the channels have been updated within the last 60 seconds.
+    If not, it makes a GET request to the server to fetch the latest channels.
+
+    Returns:
+        list: Updated channels if successful, or an error message if unsuccessful.
+
+    """
+    global CHANNELS, LAST_CHANNEL_UPDATE
+    if (
+        CHANNELS
+        and LAST_CHANNEL_UPDATE
+        and (datetime.datetime.now() - LAST_CHANNEL_UPDATE).seconds < 60
+    ):
+        return CHANNELS
+    # fetch list of channels from server
+    response = requests.get(
+        HUB_URL + "/channels", headers={"Authorization": "authkey " + HUB_AUTHKEY}
+    )
+    if response.status_code != 200:
+        return "Error fetching channels: " + str(response.text), 400
+    channels_response = response.json()
+    if not "channels" in channels_response:
+        return "No channels in response", 400
+    CHANNELS = channels_response["channels"]
+    LAST_CHANNEL_UPDATE = datetime.datetime.now()
+    return CHANNELS
+
+
+@app.route("/")
+def home_page():
+    """Render the home page displaying a list of channels.
+
+    Returns:
+        HTML: Rendered home page with the list of channels.
+
+    """
+    # fetch list of channels from server
+    return render_template("home.html", channels=update_channels())
+
+
+@app.route("/show")
+def show_channel():
+    """Render the page to show messages from a specific channel.
+
+    This function retrieves messages from the specified channel's endpoint.
+
+    Returns:
+        HTML: Rendered channel page with messages, or an error message if unsuccessful.
+
+    """
+    # fetch list of messages from channel
+    show_channel = request.args.get("channel", None)
+    if not show_channel:
+        return "No channel specified", 400
+    channel = None
+    for c in update_channels():
+        if c["endpoint"] == urllib.parse.unquote(show_channel):
+            channel = c
+            break
+    if not channel:
+        return "Channel not found", 404
+    response = requests.get(
+        channel["endpoint"], headers={"Authorization": "authkey " + channel["authkey"]}
+    )
+    if response.status_code != 200:
+        return "Error fetching messages: " + str(response.text), 400
+    messages = response.json()
+    return render_template("channel.html", channel=channel, messages=messages)
+
+
+@app.route("/post", methods=["POST"])
+def post_message():
+    """Send a message to a specified channel.
+
+    This function takes the channel, message content, and sender information
+    from the form data, and forwards the message to the channel's endpoint.
+
+    Returns:
+        Redirect: Redirects back to the channel page or an error message if unsuccessful.
+
+    """
+    # send message to channel
+    post_channel = request.form["channel"]
+    if not post_channel:
+        return "No channel specified", 400
+    channel = None
+    for c in update_channels():
+        if c["endpoint"] == urllib.parse.unquote(post_channel):
+            channel = c
+            break
+    if not channel:
+        return "Channel not found", 404
+    message_content = request.form["content"]
+    message_sender = request.form["sender"]
+    message_timestamp = datetime.datetime.now().isoformat()
+    response = requests.post(
+        channel["endpoint"],
+        headers={"Authorization": "authkey " + channel["authkey"]},
+        json={
+            "content": message_content,
+            "sender": message_sender,
+            "timestamp": message_timestamp,
+        },
+    )
+    if response.status_code != 200:
+        return "Error posting message: " + str(response.text), 400
+    return redirect(
+        url_for("show_channel") + "?channel=" + urllib.parse.quote(post_channel)
+    )
+
+
+# Start development web server
+if __name__ == "__main__":
+    app.run(port=5005, debug=True)
